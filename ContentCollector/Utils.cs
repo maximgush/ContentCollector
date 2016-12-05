0ea67ccfd4f414ee7260ae39961f255c2867fbe1
maximgush <mgush@mail.ru>
2016-12-05 19:23:01 +0700

    Функция для сравнения двух собранных контентов

--- ContentCollector/Utils.cs
+++ ContentCollector/Utils.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
 
 namespace ContentCollector
 {
@@ -22,6 +24,43 @@ namespace ContentCollector
             }
 
             return result;
+        }
+
+        static public void CompareContent(string contentFile1, string contentFile2, string resultFile)
+        {
+            HashSet<string> content1 = new HashSet<string>();
+            HashSet<string> content2 = new HashSet<string>();
+            StreamReader reader = new StreamReader(new FileStream(contentFile1, FileMode.Open));
+            while (!reader.EndOfStream)
+                content1.Add(reader.ReadLine());
+            reader.Close();
+
+            StreamReader reader2 = new StreamReader(new FileStream(contentFile2, FileMode.Open));
+            while (!reader2.EndOfStream)
+            {
+                string file = reader2.ReadLine();
+                if (content1.Contains(file))
+                    content1.Remove(file);
+                else
+                    content2.Add(file);
+            }
+            reader2.Close();
+
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(resultFile))
+            {
+                foreach (string line in content1)
+                {
+                    writer.WriteLine("+" + line);
+                }
+
+                writer.WriteLine();
+                writer.WriteLine();
+
+                foreach (string line in content2)
+                {
+                    writer.WriteLine("-" + line);
+                }
+            }
         }
     }
 }
