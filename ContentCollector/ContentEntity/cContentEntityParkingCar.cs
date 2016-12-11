af610a7d89d6bc9d53d85029c75965f0958a2216
maximgush <mgush@mail.ru>
2016-12-11 22:50:14 +0700

    Файлы для припаркованных машин

--- ContentCollector/ContentEntity/cContentEntityParkingCar.cs
+++ ContentCollector/ContentEntity/cContentEntityParkingCar.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
+using System.IO;
 using System.Xml;
 
 namespace ContentCollector
@@ -16,6 +17,19 @@ namespace ContentCollector
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         public override void Parse(cBuild build)
         {
+            string[] words = Name.Split(new char[] { '\\' });
+            string carName = words[2];
+
+            // p_player_setup.ini
+            //build.AddContentEntity(typeof(cContentEntitySimple), @"data/physics/cars/" + carName + @"\p_parking_setup.ini", this);
+
+            // n2
+            string path = build.GetManglePath("export\\gfxlib\\cars\\" + carName);
+            foreach (string file in Directory.EnumerateFiles(path, "*.n2", SearchOption.AllDirectories))
+            {
+                string name = build.GetRelativePath(file);
+                build.AddContentEntity(typeof(cContentEntityN2), name, this);
+            }
         }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     }   // cContentEntityMission
