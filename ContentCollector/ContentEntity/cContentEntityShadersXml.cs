d565013490eb4d93df985ff43b148c906ff78621
maximgush <mgush@mail.ru>
2016-12-14 09:46:00 +0700

    Добавление шейдеров и парсинг из них текстур

---
+++ ContentCollector/ContentEntity/cContentEntityShadersXml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ContentCollector
+{
+    public class cContentEntityShadersXml : cContentEntitySimple
+    {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public override void Parse(cBuild build)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(build.GetManglePath(Name));
+
+            #region Cars
+            foreach (XmlNode node in xmlDoc.GetElementsByTagName("param"))
+            {
+                if (node.Attributes["type"].Value == "Texture")
+                    build.AddContentEntity(typeof(cContentEntityTextureTga), "(logic)" + node.Attributes["def"].Value, this);                
+            }
+            #endregion
+        }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }   // сContentEntityPlayerCar
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}   // сContentCollector
